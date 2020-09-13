ddfd0007799a041501787fa0209e49e481c224d2
Nazmul Huda <mhnazmul48@gmail.com>
2020-09-13 22:51:30 +0600

    google Authentication Done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,25 @@
 import React, { useContext } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../firebaseConfig/firebaseConfig';
 
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const handleGoogleSignIn = ()=> {
+        firebase.auth().signInWithPopup(provider)
+        .then(resposne => {
+            console.log(resposne)
+        })
+        .catch(error=>{
+              console.log(error)
+          });
+    }
     return (
         <div>
-            <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Sign in With Google</button>
         </div>
     );
 };
